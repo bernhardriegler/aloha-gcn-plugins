7ef10cbf3dab33ab9786210139e1c0fcf824cd5d
bernhardriegler <b.riegler@gentics.com>
2015-08-27 19:10:58 +0200

    enhanced config - categories for tags can be added to seperate them into panels in the sidebar

--- objectproperties/lib/objectproperties-plugin.js
+++ objectproperties/lib/objectproperties-plugin.js
@@ -13,61 +13,63 @@ define([
     var plugin;
 
     var defaults = {
-        // add tags which should be reachable in sidebar here
-        tags : {
-            // title: "Title, shown in sidebar",
-            // tag: "keyword of tag"
-            // cssclass: "set a individual class for each button"
-            // checkProp: "", // property of the tag to check if not equal to checkPropValue, i.e. "text"
-            // checkPropValue: "" // value compared to checkProp i.e. "" --> tagtype text(short): (tag.part("text") !== "")
-        },
-        // markup for buttons in sidebar
+        categories : [{
+            categoryname : "Objekteigenschaften",
+            tags : [{
+                                // "title" : "<b>Suche</b>: Vor interner Suche verstecken?",
+                                // "tag" : "object.uniqa_2015_notsearchable",
+                                // "cssclass" : "sidebarobject-uniqa_2015_notsearchable",
+                                // "checkProp": "check",
+                                // "checkPropValue": false
+            }]
+        }],
         template : '<button data-tag="{tag}" class="{cssclass}">{title}</button>'
     };
+
+    var taglist = [];
 
     // update validation when clsoing tagfill
     // compare and set/remove class "is-filled"
     function validateTag($elem) {
-        // check if validation config is set
-        if($elem.attr('data-checkprop') !== undefined) {
         // focus event is fired on tagfill close (gcn-plugin.js:715)
         // handle this once
         $('body').one('focus.objectpropertiesplugin', function (e) {
-                // get the page and tag obj after tagfill close to compare changes
-                var pageId = GCNPlugin.page.id();
-                GCN.page(pageId, function (page) {
-                    // get fresh tag object
-                    page.clear();
-                    GCN.page(pageId).tag($elem.attr("data-keyword"), function(tag){
-                        console.log('run validation for ', pageId, tag.prop('id'));
-                        // get part and value to compare from html (write to html before)
-                        if (tag.part($elem.attr("data-checkprop")).toString() !== $elem.attr("data-checkpropvalue")) {
-                            // add class
-                            $elem.addClass('aloha-op-button-isfilled');
-                        } else {
-                            // not filled - remove class "is-filled"
-                            $elem.removeClass('aloha-op-button-isfilled');
-                        }
-                    });
-                });
+            // get the page and tag obj after tagfill close to compare changes
+            var pageId = GCNPlugin.page.id();
+            GCN.page(pageId, function (page){
+        // get fresh tag object
+        page.clear();
+        GCN.page(pageId).tag($elem.attr("data-keyword"), function(tag){
+                    console.log('run validation for ', pageId, tag.prop('id'));
+                    // get part and value to compare from html (write to html before)
+                    if (tag.part($elem.attr("data-checkprop")).toString() !== $elem.attr("data-checkpropvalue")) {
+                        // add class
+                    $elem.addClass('aloha-op-button-isfilled');
+                    } else {
+                        // not filled - remove class "is-filled"
+                        $elem.removeClass('aloha-op-button-isfilled');
+                    }
+        });
             });
-        }
+        });
     }
 
     function openTag($elem) {
         var pageId = GCNPlugin.page.id();
-        GCN.page(pageId).tag($elem.attr("data-keyword"), function(tag){
-            //first we have to try to close any open tag fill
-            try {
-                GCNPlugin.closeLightbox();
-            } catch(err) {
-                //tag fill could not be closed because it was not open
-            }
-            //now we can open the desired object property as tag fill
-            console.log('opening tag fill for', pageId, tag.prop('id'));
-            // hook up validation
-            validateTag($elem); 
-            GCNPlugin.openTagFill(tag.prop('id'), pageId);
+        GCN.page(pageId, function (page) {
+            page.tag($elem.attr("data-keyword"), function(tag){
+                //first we have to try to close any open tag fill
+                try {
+                    GCNPlugin.closeLightbox();
+                } catch(err) {
+                    //tag fill could not be closed because it was not open
+                }
+                //now we can open the desired object property as tag fill
+                console.log('opening tag fill for', pageId, tag.prop('id'));
+                // hook up validation
+                validateTag($elem); 
+                GCNPlugin.openTagFill(tag.prop('id'), pageId);
+            });
         });
     }
 
@@ -88,55 +90,62 @@ define([
      * Have a look at http://www.aloha-editor.org/guides/sidebar.html
      */
     function prepareSidebar(sidebar) {
-        sidebar.addPanel({
-            id: 'aloha-objectproperties-panel',
-            title: i18n.t('sidebar.objectproperties.title'),
-            expanded: true,
-            activeOn: true,
-            content: '<div id="aloha-objectproperties-panel-content">\
-                        <fieldset id="aloha-objectproperties-panel-buttoncontainer">\
-                        </fieldset>\
-                      </div>',
-            onInit: function () {
-                var $buttoncontainer = $(this.content).find('#aloha-objectproperties-panel-buttoncontainer');
-                $buttoncontainer.on('click', '.aloha-op-button', function(){
-                    openTag($(this));
-                });
-                var pageId = GCNPlugin.page.id();
+        // add panel for each category
+        for (var j = 0; j < plugin._settings.categories.length; j++) {
+            sidebar.addPanel({
+                id: 'aloha-objectproperties-panel-' + plugin._settings.categories[j].categoryname,
+                title: plugin._settings.categories[j].categoryname,
+                expanded: true,
+                activeOn: true,
+                content: '<div id="aloha-objectproperties-panel-content-' + plugin._settings.categories[j].categoryname + '">\
+                            <fieldset id="aloha-objectproperties-panel-buttoncontainer-' + plugin._settings.categories[j].categoryname + '">\
+                            </fieldset>\
+                          </div>',
+                onInit: function () {
 
-                GCN.page(pageId).tags(function(tags) {
-                    $(plugin._settings.tags).each(function(){
-                        var display = false;
-                        var filled = false;
-                        for(var i = 0; i<tags.length; i++) {
-                            if(this.tag === tags[i]._name) {
-                                display = true;
-                                // check if validation config is set
-                                if(this.hasOwnProperty('checkProp')) {
-                                    if (tags[i].part(this.checkProp) !== this.checkPropValue) {
-                                        filled = true;
+                    var pageId = GCNPlugin.page.id();
+
+                    // compare all page tags to the tags in our set
+                    GCN.page(pageId).tags(function(tags) {
+                        $(Aloha.settings.plugins.objectproperties.config).each( function (category) {
+                            var categoryname = this.categoryname;
+                            $(this.tags).each( function (){
+                                var display = false;
+                                var filled = false;
+                                for(var i = 0; i<tags.length; i++) {
+                                    if(this.tag === tags[i]._name) {
+                                        // Object Property shall be editable in sidebar
+                                        display = true;
+                                        // get container
+                                        var $buttoncontainer = $(this.content).find('#aloha-objectproperties-panel-buttoncontainer-' + categoryname);
+                                        $buttoncontainer.on('click', '.aloha-op-button', function(){
+                                            openTag($(this));
+                                        });
+                                        if (tags[i].part(this.checkProp) !== this.checkPropValue) {
+                                            filled = true;
+                                        }
                                     }
                                 }
-                            }
-                        }
-                        if(display) {
-                            var html = formatTemplate(plugin._settings.template, this);
-                            var $button = $(html);
-                            $button
-                                .attr("data-keyword", this.tag)
-                                // add validation info
-                                .attr("data-checkprop", this.checkProp)
-                                .attr("data-checkpropvalue", this.checkPropValue);
-                            $button.addClass("aloha-op-button");
-                            if(filled){
-                                $button.addClass("aloha-op-button-isfilled");
-                            }
-                            $buttoncontainer.append($button);
-                        }
+                                if(display) {
+                                    var html = formatTemplate(plugin._settings.template, this);
+                                    var $button = $(html);
+                                    $button
+                                        .attr("data-keyword", this.tag)
+                                        // add validation info
+                                        .attr("data-checkprop", this.checkProp)
+                                        .attr("data-checkpropvalue", this.checkPropValue);
+                                    $button.addClass("aloha-op-button");
+                                    if(filled){
+                                        $button.addClass("aloha-op-button-isfilled");
+                                    }
+                                    $buttoncontainer.append($button);
+                                }
+                            });
+                        });
                     });
-                });
-            }
-        });
+                }
+            });
+        }
         sidebar.show();
     }
 
